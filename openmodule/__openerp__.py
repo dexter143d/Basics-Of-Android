c967c9588a2406c8a02961bacd3fc4116d9b57fd
pankajJoshi <pankaj143342@gmail.com>
2014-03-04 15:45:00 +0530

    Update __openerp__.py

--- openmodule/__openerp__.py
+++ openmodule/__openerp__.py
@@ -1,1 +1,18 @@
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+{
+	'name': "abcGithub Integration Module",
+	'version': "1.0",
+	'author': "Dex_openERP",
+	'description': """Git integration Module""",
+    'category' : "Tools",
+	'depends': ['base'],
+	'data': [
+	    'gitapi.xml'],
+	'demo':[],
+	'installable': True,
+    	
+#'js': ['static/src/js/*.js'],
+#'css': ['static/src/css/bootstrap.min.css']
+
+
+}
+

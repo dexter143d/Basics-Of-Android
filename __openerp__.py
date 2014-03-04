18ed12abdf7ce96f5a56d2e7cf3fb8b50a95a736
pankajJoshi <pankaj143342@gmail.com>
2014-03-04 15:41:05 +0530

    module file
    empty

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+	'name': "AbcGithub Integration Module",
+	'version': "1.0",
+	'author': "Dex_openERP",
+	'description': """Git integration Module""",
+    'category' : "Tools",
+	'depends': ['base'],
+	'data': [
+	    'gitapi.xml'],
+	'demo':[],
+	'installable': True,
+    	
+#'js': ['static/src/js/*.js'],
+#'css': ['static/src/css/bootstrap.min.css']
+
+
+}
+
